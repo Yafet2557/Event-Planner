a123f9a8f2a6dab45efb3755967fbd6a58ff7075
Yafet <ytekleab2557@conestogac.on.ca>
2023-12-29 22:50:02 -0500

    Created models, url and views

--- myapi/models.py
+++ myapi/models.py
@@ -1,3 +1,50 @@
-from django.db import models
+# myapi/models.py
 
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils import timezone
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    date_of_birth = models.DateField(null=True, blank=True)
+    
+    # Add related_name to avoid clashes with default User model
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='groups',
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='customuser_set',
+        blank=True,
+        verbose_name='user permissions',
+        help_text='Specific permissions for this user.',
+    )
+
+    def __str__(self):
+        return self.username
+
+class Event(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    date = models.DateField()
+    time = models.TimeField(default=timezone.now)
+    location = models.CharField(max_length=255)
+
+    # ... other fields ...
+
+    def __str__(self):
+        return self.title
+
+class Guest(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    is_attending = models.BooleanField(default=False)
+    is_undecided = models.BooleanField(default=False)
+    is_declined = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.event.title}"
