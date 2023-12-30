6d5d9c90d58e4fba281ab308e33d8e0e6c11938b
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-30 18:02:30 -0500

    used useState to haev dynamic values for inputs.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
+import  {useState} from "react";
 
-const Register = () => {
+
+const Register = (props) => {
+  const [email, setEmail] = useState ('');
+  const [password, setPassword] = useState (''); 
+  const [number, setNumber] = useState ('');
+  const [name, setName] = useState ('');
+
+
+  const handleButton = (e) =>{
+    e.preventDefault(); // helps to not dont reload page 
+  }
+
   return (
     <div className="register">
       <h2>Register</h2>
-      <form>
+      <form onSubmit={handleButton}>
         <input type="text" placeholder="First Name" required/>
         <br/>
         <input type="text" placeholder="Last Name" required/>
@@ -23,6 +35,9 @@ const Register = () => {
       </form>
 
       <button type="submit">Register</button>
+      <label > Already have an account?</label>
+      <button onClick={() => props.onFormSwitch('login')} type="button"> Login</button>
+
     </div>
   );
 };
