8c27a647970427918786d0281069969242c661c4
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-29 20:54:23 -0500

    imported login.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import HelloWorld from './HelloWorld';
 import Navbar from './navbar';
 import './index';
-
+import Login from './login';
 
 function App() {
   return (
     <div>
       <Navbar/>
+      <Login />
       <HelloWorld />
     </div>
   );
