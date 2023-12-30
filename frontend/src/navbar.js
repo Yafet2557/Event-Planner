5a717cb80a9b300921a3b90875a84d8672483200
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-29 20:45:56 -0500

    added 2 navbar elements

---
+++ frontend/src/navbar.js
@@ -0,0 +1,12 @@
+const Navbar = () => {
+    return ( 
+            <nav className="navbar">
+                <div className="links">
+                    <a href="/Home"> Home</a>
+                    <a href="/Create"> New Event</a>
+                </div>
+            </nav>
+     );
+}
+ 
+export default Navbar;
