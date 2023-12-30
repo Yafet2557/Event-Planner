170fc6be9281535c0559f7eb7c988488fafdc46f
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-29 20:46:30 -0500

    removed uncessary imports.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import Navbar from './navbar';
+import './index';
+
 
 function App() {
   return (
     <div>
+      <Navbar/>
       <HelloWorld />
     </div>
   );
