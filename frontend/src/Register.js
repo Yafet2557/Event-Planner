06213bddf01399a56298d3fd8d582cfc486ef94b
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-30 18:45:59 -0500

    better component.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -37,7 +37,7 @@ const Register = (props) => {
       <button type="submit">Register</button>
       <label > Already have an account?</label>
       <button onClick={() => props.onFormSwitch('login')} type="button"> Login</button>
-
+      <br/>
     </div>
   );
 };
