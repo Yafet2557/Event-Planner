e925997c51528e7200578fb711ae88019843a834
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-30 20:22:32 -0500

    added call to backend server.

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -1,40 +1,69 @@
 import React from 'react';
-import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import  {useState} from "react";
+import axios from 'axios';
+
 
 
 const Register = (props) => {
-  const [email, setEmail] = useState ('');
-  const [password, setPassword] = useState (''); 
-  const [number, setNumber] = useState ('');
-  const [name, setName] = useState ('');
+  const [isRegistered, setIsRegistered] = useState(false);
 
 
-  const handleButton = (e) =>{
-    e.preventDefault(); // helps to not dont reload page 
+  const [ inputs, setInputs] = useState({
+    first_name: '',
+    last_name:'',
+    email:'',
+    password:'',
+    date_of_birth:'',
+    phone_number:''});
+    const inputChange = (e) => { setInputs({...inputs, [e.target.name]: e.target.value});
+    };
+
+  {/* FORM SUBMISSION  */}
+  const submitFormButton = (e) =>{
+    e.preventDefault(); // helps to not reload page
+    setIsRegistered(true);
+    CallServer(inputs);
   }
+
+  {/* API CALL  */}
+  const CallServer = (inputs) => {
+    axios.post ('http://127.0.0.1:8000/api/register_user/', inputs)
+    .then(response => {
+      console.log('SUCESFULLY CALLED API' + response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }  
 
   return (
     <div className="register">
       <h2>Register</h2>
-      <form onSubmit={handleButton}>
-        <input type="text" placeholder="First Name" required/>
+      <form onSubmit={submitFormButton}>
+        
+      {/* INPUTS BUTTON  */}
+        <input type="text" value={inputs.first_name} placeholder="First Name" required name="first_name" onChange={inputChange}/>
         <br/>
-        <input type="text" placeholder="Last Name" required/>
+        <input type="text" value={inputs.last_name} placeholder="Last Name" required name="last_name" onChange={inputChange}/>
         <br/>
-        <input type="text" placeholder="#Phone number" required/>
+        <input type="text" value={inputs.phone_number}  placeholder="#Phone number" required  name="phone_number" onChange={inputChange}/>
         <br/>
-        <input type="text" placeholder="Date Of Birth" required/>
+        <input type="text" value={inputs.date_of_birth} placeholder="Date Of Birth" required  name="date_of_birth" onChange={inputChange}/>
         <br/>
-        <input type="email" placeholder="Email@" required/>
+        <input type="email" value={inputs.email} placeholder="Email@" required name="email" onChange={inputChange}/>
         <br/>
-        <input type="password" placeholder="Password" required/>
+        <input type="password" value={inputs.password} placeholder="Password" required name="password" onChange={inputChange}/>
         <br/>
-        <input type="password" placeholder="Confirm Password" required/>
+
+      {/* REGISTER */}
+      { isRegistered===false && <button type="submit">Register</button>}
+      {isRegistered && <span id="confirmation">You're signed up</span>} <br/>
       </form>
 
-      <button type="submit">Register</button>
+
+
+      {/* LOGIN BUTTON  */}
       <label > Already have an account?</label>
       <button onClick={() => props.onFormSwitch('login')} type="button"> Login</button>
       <br/>
