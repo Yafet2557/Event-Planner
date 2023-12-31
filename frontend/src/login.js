3107901ab49c787ecd3c3a2403e37fbd4d3d9b85
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2023-12-30 19:14:34 -0500

    added icons beside inputs.

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,4 +1,7 @@
 import React, {useState} from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLock } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 
 
 const Login = (props) => {
@@ -12,18 +15,23 @@ const Login = (props) => {
   }
 
     return (
+      
       <div className="register">
           <h2>Login</h2>
           <form onSubmit={handleButton}>
+
+          <FontAwesomeIcon icon={faEnvelope} bounce size="sm" style={{color: "#2d59a4",}} />
             <label htmlFor="email"> Email</label>
             <input  type="email" placeholder="youremail@example.com" id="email" name="email"/>
             <br/>
 
+
+            <FontAwesomeIcon icon={faLock} bounce size="sm" style={{color: "#2d59a4",}} />
             <label htmlFor="password"> Password</label>
             <input type="password" placeholder="**********" id="password" name="password"/>
 
             <button >Login</button>
-
+            <i class="fa-solid fa-lock"></i>
             <label > Don't have an account?</label>
             <button onClick={() => props.onFormSwitch('Register')} type="button"> Register</button>
             <br/>
