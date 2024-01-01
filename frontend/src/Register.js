c1def367f28d097292e28afa501d8458aaa7473e
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2024-01-01 17:27:29 -0500

    .

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -41,35 +41,37 @@ const Register = (props) => {
   }  
 
   return (
-    <div className="register">
-      <h2>Register</h2>
-      <form onSubmit={submitFormButton}>
-        
-      {/* INPUTS BUTTON  */}
-        <input type="text" value={inputs.first_name} placeholder="First Name" required name="first_name" onChange={inputChange}/>
-        <br/>
-        <input type="text" value={inputs.last_name} placeholder="Last Name" required name="last_name" onChange={inputChange}/>
-        <br/>
-        <input type="text" value={inputs.phone_number}  placeholder="#Phone number" required  name="phone_number" onChange={inputChange}/>
-        <br/>
-        <input type="text" value={inputs.date_of_birth} placeholder="Date Of Birth" required  name="date_of_birth" onChange={inputChange}/>
-        <br/>
-        <input type="email" value={inputs.email} placeholder="Email@" required name="email" onChange={inputChange}/>
-        <br/>
-        <input type="password" value={inputs.password} placeholder="Password" required name="password" onChange={inputChange}/>
-        <br/>
+    <div className="HomePage"> 
+      <div className="register">
+        <h2>Register</h2>
+        <form onSubmit={submitFormButton}>
+          
+        {/* INPUTS BUTTON  */}
+          <input type="text" value={inputs.first_name} placeholder="First Name" required name="first_name" onChange={inputChange}/>
+          <br/>
+          <input type="text" value={inputs.last_name} placeholder="Last Name" required name="last_name" onChange={inputChange}/>
+          <br/>
+          <input type="text" value={inputs.phone_number}  placeholder="#Phone number" required  name="phone_number" onChange={inputChange}/>
+          <br/>
+          <input type="text" value={inputs.date_of_birth} placeholder="Date Of Birth" required  name="date_of_birth" onChange={inputChange}/>
+          <br/>
+          <input type="email" value={inputs.email} placeholder="Email@" required name="email" onChange={inputChange}/>
+          <br/>
+          <input type="password" value={inputs.password} placeholder="Password" required name="password" onChange={inputChange}/>
+          <br/>
 
-      {/* REGISTER */}
-      { isRegistered===false && <button type="submit">Register</button>}
-      {isRegistered && <span id="confirmation">You're signed up</span>} <br/>
-      </form>
+        {/* REGISTER */}
+        { isRegistered===false && <button type="submit">Register</button>}
+        {isRegistered && <span id="confirmation">You're signed up</span>} <br/>
+        </form>
 
 
 
-      {/* LOGIN BUTTON  */}
-      <label > Already have an account?</label>
-      <button onClick={() => props.onFormSwitch('login')} type="button"> Login</button>
-      <br/>
+        {/* LOGIN BUTTON  */}
+        <label > Already have an account?</label>
+        <button onClick={() => props.onFormSwitch('login')} type="button"> Login</button>
+        <br/>
+      </div>
     </div>
   );
 };
