0cb29a439eb3ab402858b5d94785b7767a02963d
Almonaabdo <109709044+Almonaabdo@users.noreply.github.com>
2024-01-01 21:43:50 -0500

    added Home tap.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -19,6 +19,10 @@ const router = createBrowserRouter([
     element: <Home/>,
   },
   {
+    path: "/Home",
+    element: <Home/>,
+  },
+  {
     path: "/Login",
     element: <Login/>,
   },
@@ -32,7 +36,3 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <RouterProvider router={router} />
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
