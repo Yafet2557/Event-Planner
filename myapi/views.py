71c54d3bc1a158eb8116310b8f7b09d2ac30c02b
Yafet <ytekleab2557@conestogac.on.ca>
2024-01-08 19:56:06 -0500

    Created new views for crud operations and utilized serialiazer.

--- myapi/views.py
+++ myapi/views.py
@@ -1,71 +1,138 @@
-# views.py
-
-import random
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.hashers import check_password
+from django.contrib.auth import login, logout
 from django.http import JsonResponse
-from rest_framework.decorators import api_view, permission_classes
 from rest_framework import status
-from .models import CustomUser, Event
+from rest_framework.authtoken.models import Token
+from rest_framework.generics import RetrieveAPIView, CreateAPIView, DestroyAPIView,ListAPIView,UpdateAPIView
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.decorators import login_required
+from rest_framework.views import APIView
+
+from .models import Event
+from .serializers import UserSeriliazer, EventSerializer, UserLoginSerializer
 
 
-@api_view(['POST'])
-def register_user(request):
-    data = request.data
-    firstname = data.get('first_name')
-    lastname = data.get('last_name')
-    email = data.get('email')
-    password = data.get('password')
-    phone = data.get('phone_number')
+class RegisterUserAPIView(APIView):
+    """
+    API endpoint for user registration.
 
-    # Generate a unique username based on the first name and random numbers
-    random_numbers = ''.join(str(random.randint(0, 9)) for _ in range(3))
-    username = f"{firstname}_{random_numbers}"
+    Accepts a POST request with user registration data.
+    
+    Returns a JSON response indicating success or failure.
 
-    try:
-        user = CustomUser.objects.create_user(
-            username=username, first_name=firstname, last_name=lastname, email=email, password=password,
-            phone_number=phone
-        )
-        return JsonResponse({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    """
 
+    http_method_names = ['post']
 
-@api_view(['POST', 'GET'])
-def user_login(request):
-    if request.method == 'POST':
+    def post(self, request):
         data = request.data
-        email = data.get('email')
-        password = data.get('password')
+        serializer = UserSeriliazer(data=data)
 
-        # Authenticate user
-        user = authenticate(username=email, password=password)
-
-        if user is not None:
-            # User authenticated successfully
-            login(request, user)
-            return JsonResponse({'message': 'Login successful'}, status=status.HTTP_200_OK)
+        if serializer.is_valid():
+            user = serializer.save()
+            # login(request, user) not sure about this yet
+            token, created = Token.objects.get_or_create(user=user)
+            return JsonResponse({'message': 'User registered successfully'}, status=status.HTTP_201_CREATED)
         else:
-            # User authentication failed
-            return JsonResponse({'message': 'Invalid login credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-    else:
-        # Method not allowed
-        return JsonResponse({'message': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+            return JsonResponse({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
 
-@api_view(['POST', 'GET'])
-@login_required
-def create_event(request):
-    data = request.data
-    print(data)
-    data['creator'] = request.user
-    try:
-        event = Event.objects.create(**data)
-        print(event)
-        return JsonResponse({'message': 'Event created successfully'}, status=status.HTTP_201_CREATED)
-    except Exception as e:
-        return JsonResponse({'message': f'Error creating event: {str(e)}'},
-                            status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+class UserLoginAPIView(APIView):
+    """
+    API endpoint for user login.
+
+    Accepts a POST request with user login credentials.
+    
+    Returns a JSON response indicating success or failure including the authentication token.
+
+    """
+
+    http_method_names = ['post']
+
+    @staticmethod
+    def post(request: object) -> object:
+
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.validated_data['user']
+            login(request, user)
+            token, created = Token.objects.get_or_create(user=user)
+            return JsonResponse({'message': 'Login successful', 'token': token.key}, status=status.HTTP_200_OK)
+        else:
+            return JsonResponse({'error': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLogoutAPIView(APIView):
+    http_method_names = ['post']
+
+    def post(self, request):
+        logout(request)
+        return JsonResponse({'message': 'Logged out successfully'}, status=status.HTTP_200_OK)
+
+
+class CreateEventAPIView(CreateAPIView):
+    """
+    API endpoint for creating events.
+
+    Accepts a POST request with event data.
+    
+    Requires authentication token in the headers for the logged-in user.
+    
+    Returns a JSON response indicating success or failure.
+    
+    """
+
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]  # Only allows authenticated users to make requests
+
+    # def perform_create(self, serializer):
+    # serializer.save(creator=self.request.user)
+
+
+# def create(self, request):
+#     super().create(request)
+#    return JsonResponse({'Event':'Created successfully'}, status=status.HTTP_201_CREATED)
+
+
+class EventDetailsAPIView(RetrieveAPIView):
+    http_method_names = ['get']
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, pk):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return JsonResponse({'event': serializer.data}, status=status.HTTP_200_OK)
+
+class DeleteEventAPIView(DestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]
+
+class ListEventsAPIView(ListAPIView):
+    http_method_names = ['get']
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes = [IsAuthenticated]    
+
+class UpdateEventDetails(UpdateAPIView):
+    http_method_names = ['patch']     # Define allowed HTTP methods
+
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+    permission_classes= [IsAuthenticated]
+
+    def perform_update(self, serializer):
+        field = self.request.data.get('new_value')
+        if field:
+            serializer.save(**{field:self.request.data.get(field)})
+        else:
+            serializer.save()
+
+    def update(self, request, **kwargs):
+        partial = kwargs.pop('partial',False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance,data=request.data,partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        return JsonResponse({'Data':serializer.data})
